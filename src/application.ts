8ea3473dfba90933939f4d70b33c4a982581998f
botre <git@bjornkrols.com>
2022-04-23 11:55:13 +0200

    Initial commit

---
+++ src/application.ts
@@ -0,0 +1,21 @@
+import express from "express";
+
+const application = express();
+
+const port = 8080;
+
+application
+  .get("/", (req, res) => {
+    res.send({
+      message: "Hello, World!",
+    });
+  })
+  .get("/random", (req, res) => {
+    res.send({
+      number: Math.floor(Math.random() * 100),
+    });
+  });
+
+application.listen(port, () => {
+  console.log(`Application listening on port ${port}`);
+});
